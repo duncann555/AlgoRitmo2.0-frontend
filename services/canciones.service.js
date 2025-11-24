5c0181e2bf2ec12b0502d8bcfb2a872328db5a6a
Seba Flomenbaun <sebaflomen@gmail.com>
2025-11-23 22:42:41 -0300

    cambie variable const BASE

--- services/canciones.service.js
+++ services/canciones.service.js
@@ -1,6 +1,6 @@
 import { fromApiSong, toApiSong } from "./mappers";
 
-const BASE = "https://algo-ritmo2-0-backend.vercel.app/api";
+const BASE = "https://algo-ritmo2-0-backend.vercel.app/api/canciones";
 
 export const getCanciones = async () => {
   const r = await fetch(BASE);
@@ -14,8 +14,10 @@ export const crearCancionApi = async (songFront) => {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(toApiSong(songFront)),
   });
+
   const data = await r.json();
   if (!r.ok) throw new Error(data.mensaje || "Error creando canción");
+
   return fromApiSong(data.cancion);
 };
 
@@ -25,14 +27,20 @@ export const editarCancionApi = async (id, songFront) => {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(toApiSong(songFront)),
   });
+
   const data = await r.json();
   if (!r.ok) throw new Error(data.mensaje || "Error editando canción");
+
   return fromApiSong(data.cancion);
 };
 
 export const borrarCancionApi = async (id) => {
-  const r = await fetch(`${BASE}/${id}`, { method: "DELETE" });
+  const r = await fetch(`${BASE}/${id}`, {
+    method: "DELETE",
+  });
+
   const data = await r.json();
   if (!r.ok) throw new Error(data.mensaje || "Error borrando canción");
+
   return true;
 };
